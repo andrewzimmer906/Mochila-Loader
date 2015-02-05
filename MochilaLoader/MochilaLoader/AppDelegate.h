feda5690796b4dd10b47b11f58e0b7f88cd2a400
Andrew Zimmer <andrew@revolv.com>
2015-02-05 12:58:50 -0700

    initial commit

---
+++ MochilaLoader/MochilaLoader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MochilaLoader
+//
+//  Created by Andrew Zimmer on 2/5/15.
+//  Copyright (c) 2015 AndrewZimmer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MochilaLoader/MochilaLoader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MochilaLoader
+//
+//  Created by Andrew Zimmer on 2/5/15.
+//  Copyright (c) 2015 AndrewZimmer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
